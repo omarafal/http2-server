5ac2520d807879e17dc2d08ea61c31f2d5a2734b
Omar Ashraf <114801560+omarafal@users.noreply.github.com>
2024-12-21 00:35:52 +0200

    add misc

--- misc.py
+++ misc.py
@@ -1,10 +1,31 @@
 from datetime import datetime
+from termcolor import colored
+import base64
 
-def print_cmd(string):
+def b64_decode(msg):
+    """
+    Decode base64 encoded messages and return it in pure string format
+    """
+    return str(base64.b64decode(msg.replace("b", "", 1).replace("\'", "", 2))).replace("b", "", 1).replace("\'", "", 2)
+
+def make_frame(header):
+    
+    """
+    Function to create request from dictionaries provided
+    """
+
+    frame = ""
+    for key, value in header.items():
+        frame += f"{key}: {value}\r\n"
+
+    return frame
+
+def print_cmd(data, header=None):
     """
     Format printing
     """
-    print(f"[{datetime.now()}] {string}")
+    print(f"[{datetime.now()}] {data}" if header==None else f"[{datetime.now()}] {colored(header, "red")}\n{data}")
+
 
 def print_debug(string):
     """
